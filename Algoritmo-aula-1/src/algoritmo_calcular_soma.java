cb3a0dc7773d889c9eb475cbe58ddbebb2e4991e
Hugo G.Fernandes <hugocodingdev@gmail.com>
2025-04-10 14:17:55 -0300

    Adicionado gitignore

---
+++ Algoritmo-aula-1/src/algoritmo_calcular_soma.java
@@ -0,0 +1,21 @@
+public class algoritmo_calcular_soma {
+    public static void main(String[] args) {
+
+
+        int n1, n2, resultado;
+
+
+        n1 = 3;
+        n2 = 5;
+        resultado = n1 + n2;
+
+
+
+        System.out.println("A soma de "
+                + n1 + " + " + n2 + " e: "
+                + resultado);
+
+
+
+    }
+}
