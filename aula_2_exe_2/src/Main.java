63abcd4433dc440ec4550ec8bcfdce4ceac6015c
Hugo G.Fernandes <hugocodingdev@gmail.com>
2025-04-10 23:20:21 -0300

    Adicionado aula-2-exe-2

---
+++ aula_2_exe_2/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int som, sub, mul, div;
+
+
+
+        som = 3+3;
+        sub = 6-4;
+        mul = 2*2;
+        div = 6/2;
+
+
+
+        System.out.println("A soma 3 + 3 e: " + som);
+        System.out.println("A subtracao 6 - 4 e: " + sub);
+        System.out.println("A multiplicacao de 2 * 2 e: " + mul);
+        System.out.println("A divisao de 6 / 2 e: " + div);
+    }
+}
