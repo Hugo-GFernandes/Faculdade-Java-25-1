5c44cb83293104a96d982bdc3e255971e5c5ed1b
Hugo G.Fernandes <hugocodingdev@gmail.com>
2025-04-10 22:46:53 -0300

    Adicionado aula-2-ex-1

---
+++ Aula-2-exe-1/src/Aula_2_exe_1.java
@@ -0,0 +1,24 @@
+public class Aula_2_exe_1 {
+    public static void main(String[] args) {
+
+
+        int a;
+        float b;
+        String c;
+        boolean d;
+
+
+
+        a = 5;
+        b = 3.87f;
+        c = "hello";
+        d = true;
+
+
+        System.out.println("Este e meu numero inteiro: " + a);
+        System.out.println("Este e meu numero Real: " + b);
+        System.out.println("Este e meu caractere: " + c);
+        System.out.println("Este e meu booleano: " + d);
+
+    }
+}
